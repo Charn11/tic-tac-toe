4ff5ca06e0b35dd639951c184ad50c97ae0852ee
guru <gc66357@gmail.com>
2022-09-04 21:50:00 +0530

    edit gameplay

--- script.js
+++ script.js
@@ -16,19 +16,15 @@ const TicTacToeModule = ( () => {
             document.getElementById('grid-cont').appendChild(grid_elm);
         }
 
-        //store player name in object
-        const player1 = playerFactory(validate1.value,0);
-        const player2 = playerFactory(validate2.value,0);
-        console.log(player2.playerName);
-        console.log(player1.playerName);
+        createObject();
 
         //change score player name on click of start button
         let scr1 = document.getElementById('scr1');
-        scr1.innerText = player1.playerName+"(X): 0";
+        scr1.innerText = createObject(1)+"(X): 0";
         let scr2 = document.getElementById('scr2');
-        scr2.innerText = player2.playerName+"(O): 0";
+        scr2.innerText = createObject(2)+"(O): 0";
     });
-
+    
     //listens for XO player click in gameboard
     let playerMove = document.getElementsByClassName('grid-elm');
     let count = 0;
@@ -49,8 +45,6 @@ const TicTacToeModule = ( () => {
     })
 
     //validate submit button
-    const validate1 = document.getElementById('player1');
-    const validate2 = document.getElementById('player2');
     const form = document.getElementById('playerNames')
     form.addEventListener('input', e => {
         if(validate1.value!=""&&validate2.value!="")
@@ -59,27 +53,54 @@ const TicTacToeModule = ( () => {
         }
     })
 
-    //gameplay logic
-    let b1 = document.getElementById('b1'), b2 = document.getElementById('b2'), b3 = document.getElementById('b3'), 
-    b4 = document.getElementById('b4'), b5 = document.getElementById('b5'), b6 = document.getElementById('b6'), 
-    b7 = document.getElementById('b7'), b8 = document.getElementById('b8'), b9 = document.getElementById('b9');
-    
-    document.addEventListener('click', e=> {
-      
-        let classList = document.getElementsByClassName("grid-elm");
-        let s = [];
-        for(let i=0; i<classList.length; i++)
-        {
-            s[i]=classList[i].innerText;
-            if(s[0]=="X"&&s[1]=="X"&&s[2]=="X")
-            {
-                alert("yes");
-            }
-        }
-      
-    })
-
 })();
+
+//gameplay logic
+let b1 = document.getElementById('b1'), b2 = document.getElementById('b2'), b3 = document.getElementById('b3'), 
+b4 = document.getElementById('b4'), b5 = document.getElementById('b5'), b6 = document.getElementById('b6'), 
+b7 = document.getElementById('b7'), b8 = document.getElementById('b8'), b9 = document.getElementById('b9');
+
+document.addEventListener('click', e=> {
+  
+    let classList = document.getElementsByClassName("grid-elm");
+    let s = [];
+    for(let i=0; i<classList.length; i++)
+    {
+        s[i]=classList[i].innerText;
+        if((s[0]=="X"&&s[1]=="X"&&s[2]=="X")||(s[0]=="X"&&s[3]=="X"&&s[6]=="X")||(s[0]=="X"&&s[4]=="X"&&s[8]=="X"))
+        {
+            alert(createObject(1)+" wins");
+            document.getElementById("scr1").innerText = createObject(1)+"(X): "+createObject(11);
+
+        }
+    }
+  
+})
+
+const validate1 = document.getElementById('player1');
+const validate2 = document.getElementById('player2');
+
+//store player name in object
+function createObject(flag)
+{
+    const player1 = playerFactory(validate1.value,0);
+    const player2 = playerFactory(validate2.value,0);
+    console.log(player2.playerName);
+    console.log(player1.playerName);
+    if(flag==1)
+    {
+        return player1.playerName;
+    }
+    else if(flag==11)
+    {
+        player1.score+=1;
+        return player1.score;
+    }
+    else
+    {
+        return player2.playerName;
+    }
+}
 
 const playerFactory = (playerName, score) => {
     return {playerName: playerName, score: score};
