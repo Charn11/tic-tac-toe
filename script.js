c61c6bf998efe6b249a1f93fbebe62bbd4d91dd6
guru <gc66357@gmail.com>
2022-09-05 15:17:45 +0530

    edit logic

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const TicTacToeModule = ( () => {
 
     let startGame = document.getElementById('start-button');
 
+    const validate1 = document.getElementById('player1');
+    const validate2 = document.getElementById('player2');
+
     startGame.addEventListener('click', e => {
         let grid_container = document.createElement('div');
         grid_container.setAttribute('id','grid-cont');
@@ -16,30 +19,38 @@ const TicTacToeModule = ( () => {
             document.getElementById('grid-cont').appendChild(grid_elm);
         }
 
-        createObject();
+        let round = document.createElement('div');
+        round.setAttribute('id','round');
+        document.getElementById('board-selector').appendChild(round);
+        round.innerText = "Round 1";
+        
+        //store player name in object
+        player1 = playerFactory(validate1.value,0);
+        player2 = playerFactory(validate2.value,0);
+        console.log(player2.playerName);
+        console.log(player1.playerName);
 
         //change score player name on click of start button
         let scr1 = document.getElementById('scr1');
-        scr1.innerText = createObject(1)+"(X): 0";
+        scr1.innerText = player1.playerName+"(X): 0";
         let scr2 = document.getElementById('scr2');
-        scr2.innerText = createObject(2)+"(O): 0";
+        scr2.innerText = player2.playerName+"(O): 0";
     });
     
     //listens for XO player click in gameboard
     let playerMove = document.getElementsByClassName('grid-elm');
-    let count = 0;
     document.addEventListener('click', e => {
         if(e.target.className=="grid-elm"&&e.target.innerText=="")
         {  
-            if(count%2!=0)
+            if(resetCount%2!=0)
             {
                 e.target.innerText = "O";
-                count++;
+                resetCount++;
             }
             else
             {
                 e.target.innerText = "X";
-                count++;
+                resetCount++;
             }
         }
     })
@@ -55,6 +66,9 @@ const TicTacToeModule = ( () => {
 
 })();
 
+let player1, player2;
+let stage = 1;
+let resetCount = 0;
 //gameplay logic
 let b1 = document.getElementById('b1'), b2 = document.getElementById('b2'), b3 = document.getElementById('b3'), 
 b4 = document.getElementById('b4'), b5 = document.getElementById('b5'), b6 = document.getElementById('b6'), 
@@ -69,39 +83,30 @@ document.addEventListener('click', e=> {
         s[i]=classList[i].innerText;
         if((s[0]=="X"&&s[1]=="X"&&s[2]=="X")||(s[0]=="X"&&s[3]=="X"&&s[6]=="X")||(s[0]=="X"&&s[4]=="X"&&s[8]=="X"))
         {
-            alert(createObject(1)+" wins");
-            document.getElementById("scr1").innerText = createObject(1)+"(X): "+createObject(11);
-
+            alert(player1.playerName);
+            document.getElementById("round").innerText = "Round "+stage;
+            resetBoard();
+            break;
         }
     }
   
 })
 
-const validate1 = document.getElementById('player1');
-const validate2 = document.getElementById('player2');
-
-//store player name in object
-function createObject(flag)
+function resetBoard()
 {
-    const player1 = playerFactory(validate1.value,0);
-    const player2 = playerFactory(validate2.value,0);
-    console.log(player2.playerName);
-    console.log(player1.playerName);
-    if(flag==1)
+    let reset = document.getElementsByClassName("grid-elm");
+    for(let i=0; i<reset.length;i++)
     {
-        return player1.playerName;
-    }
-    else if(flag==11)
-    {
-        player1.score+=1;
-        return player1.score;
-    }
-    else
-    {
-        return player2.playerName;
+        reset[i].innerText = "";
+        resetCount = 0;
     }
 }
 
+
 const playerFactory = (playerName, score) => {
-    return {playerName: playerName, score: score};
+    const returnName = () => 
+    {
+        alert(this.playerName);
+    }
+    return {playerName, score, returnName};
 }
