63e4f68399b3822deb910aae960fb59690980e9a
guru <gc66357@gmail.com>
2022-09-06 01:04:25 +0530

    edit gameplay

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Module
+// Initialize Module
 const TicTacToeModule = ( () => {
 
     let startGame = document.getElementById('start-button');
@@ -24,6 +24,21 @@ const TicTacToeModule = ( () => {
         document.getElementById('board-selector').appendChild(round);
         round.innerText = "Round 1";
         
+        let restart = document.createElement('button');
+        restart.setAttribute('id','restart');
+        restart.innerText = "Restart";
+        document.getElementById('board-selector').appendChild(restart);
+
+        //restart listener
+        restart.addEventListener("click", e => {
+            let resetGrid = document.getElementsByClassName("grid-elm");
+            resetCount = 0;
+            for(let i=0; i<resetGrid.length;i++)
+            {
+                resetGrid[i].innerText = "";
+            }
+        })
+
         //store player name in object
         player1 = playerFactory(validate1.value,0);
         player2 = playerFactory(validate2.value,0);
@@ -40,7 +55,7 @@ const TicTacToeModule = ( () => {
     //listens for XO player click in gameboard
     let playerMove = document.getElementsByClassName('grid-elm');
     document.addEventListener('click', e => {
-        if(e.target.className=="grid-elm"&&e.target.innerText=="")
+        if(e.target.className=="grid-elm"&&e.target.innerText==""&&gameCount==0)
         {  
             if(resetCount%2!=0)
             {
@@ -69,43 +84,129 @@ const TicTacToeModule = ( () => {
 let player1, player2;
 let stage = 1;
 let resetCount = 0;
+let gameCount = 0;
 
-//gameplay logic
-let b1 = document.getElementById('b1'), b2 = document.getElementById('b2'), b3 = document.getElementById('b3'), 
-b4 = document.getElementById('b4'), b5 = document.getElementById('b5'), b6 = document.getElementById('b6'), 
-b7 = document.getElementById('b7'), b8 = document.getElementById('b8'), b9 = document.getElementById('b9');
-
-document.addEventListener('click', e=> {
-  
-    let classList = document.getElementsByClassName("grid-elm");
-    let s = [];
-    for(let i=0; i<classList.length; i++)
-    {
-        s[i]=classList[i].innerText;
-        if((s[0]=="X"&&s[1]=="X"&&s[2]=="X")||(s[0]=="X"&&s[3]=="X"&&s[6]=="X")||(s[0]=="X"&&s[4]=="X"&&s[8]=="X"))
+//Gameplay Module
+const Gameplay = ( () => {
+    document.addEventListener('click', e=> {
+    
+        let classList = document.getElementsByClassName("grid-elm");
+        let s = [];
+        for(let i=0; i<classList.length; i++)
         {
-            alert(player1.playerName+" wins");
+            s[i]=classList[i].innerText;
+            if((s[0]=="X"&&s[1]=="X"&&s[2]=="X")||(s[0]=="X"&&s[3]=="X"&&s[6]=="X")||(s[0]=="X"&&s[4]=="X"&&s[8]=="X")||
+            (s[1]=="X"&&s[4]=="X"&&s[7]=="X")||(s[2]=="X"&&s[4]=="X"&&s[6]=="X")||(s[2]=="X"&&s[5]=="X"&&s[8]=="X")||
+            (s[3]=="X"&&s[4]=="X"&&s[5]=="X")||(s[6]=="X"&&s[7]=="X"&&s[8]=="X"))
+            {
+                document.body.style.pointerEvents = "none"
+                gameCount = 1;
+                player1.score++;
+                document.getElementById("scr1").innerText = player1.playerName+"(X): "+player1.score;
+                let nextRound = document.createElement('div');
+                nextRound.setAttribute('id','next');
+                nextRound.innerText = player1.playerName+" wins!";
+                document.getElementById('board-selector').appendChild(nextRound);
+                document.getElementById('restart').remove();
+                addButtons();
+                break;
+            }
+            else if((s[0]=="O"&&s[1]=="O"&&s[2]=="O")||(s[0]=="O"&&s[3]=="O"&&s[6]=="O")||(s[0]=="O"&&s[4]=="O"&&s[8]=="O")||
+            (s[1]=="O"&&s[4]=="O"&&s[7]=="O")||(s[2]=="O"&&s[4]=="O"&&s[6]=="O")||(s[2]=="O"&&s[5]=="O"&&s[8]=="O")||
+            (s[3]=="O"&&s[4]=="O"&&s[5]=="O")||(s[6]=="O"&&s[7]=="O"&&s[8]=="O"))
+            {
+                gameCount = 1;
+                player2.score++;
+                document.getElementById("scr2").innerText = player2.playerName+"(O): "+player2.score;
+                let nextRound = document.createElement('div');
+                nextRound.setAttribute('id','next');
+                nextRound.innerText = player2.playerName+" wins!";
+                document.getElementById('board-selector').appendChild(nextRound);
+                document.getElementById('restart').remove();
+                addButtons();
+                break;
+            }
+        }
+    })
+
+    /*function logic(a)
+    {
+        if(a==1)
+        {
             player1.score++;
             document.getElementById("scr1").innerText = player1.playerName+"(X): "+player1.score;
+            let nextRound = document.createElement('div');
+            nextRound.setAttribute('id','next');
+            nextRound.innerText = player1.playerName+" wins!";
+            document.getElementById('board-selector').appendChild(nextRound);
+            document.getElementById('restart').remove();
+            document.body.style.pointerEvents = "none"
+            addButtons();
+        }
+        else if(a==2){
+            player2.score++;
+            document.getElementById("scr2").innerText = player2.playerName+"(O): "+player2.score;
+            let nextRound = document.createElement('div');
+            nextRound.setAttribute('id','next');
+            nextRound.innerText = player2.playerName+" wins!";
+            document.getElementById('board-selector').appendChild(nextRound);
+            document.getElementById('restart').remove();
+            addButtons();
+        }
+    }*/
+
+    function addButtons(){
+        let nextButton = document.createElement('button');
+        let resetButton = document.createElement('button');
+        nextButton.setAttribute('id','nxtButton');
+        resetButton.setAttribute('id','resetButton');
+        nextButton.innerText = "Next round";
+        resetButton.innerText = "Reset";
+        document.getElementById('board-selector').appendChild(nextButton);
+        document.getElementById('board-selector').appendChild(resetButton);
+        nextButton.addEventListener('click', e=> {
             stage++;
             document.getElementById("round").innerText = "Round "+stage;
+            addRestartButton();
             resetBoard();
-            break;
-        }
+            })
+        resetButton.addEventListener('click', e => {
+        location.reload();
+            })
     }
-})
 
-function resetBoard()
-{
-    let reset = document.getElementsByClassName("grid-elm");
-    for(let i=0; i<reset.length;i++)
+    function addRestartButton()
     {
-        reset[i].innerText = "";
-        resetCount = 0;
+        let restart = document.createElement('button');
+        restart.setAttribute('id','restart');
+        restart.innerText = "Restart";
+        document.getElementById('board-selector').appendChild(restart);
+        restart.addEventListener("click", e => {
+            let resetGrid = document.getElementsByClassName("grid-elm");
+            resetCount = 0;
+            for(let i=0; i<resetGrid.length;i++)
+            {
+                resetGrid[i].innerText = "";
+            }
+        })
     }
-}
 
+    function resetBoard()
+    {
+    let resetGrid = document.getElementsByClassName("grid-elm");
+    resetCount = 0;
+    for(let i=0; i<resetGrid.length;i++)
+        {
+            resetGrid[i].innerText = "";
+        }
+        document.getElementById('nxtButton').remove();
+        document.getElementById('resetButton').remove();
+        document.getElementById('next').remove();
+        gameCount = 0;
+    }
+}) ();
 
+//factory
 const playerFactory = (playerName, score) => {
     return {playerName, score};
 }
